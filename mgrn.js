4a52e2db035a4186a2c1d0248b463f1cb5503ab9
james <zetlen@gmail.com>
2014-05-17 23:22:56 -0500

    adding random side

--- mgrn.js
+++ mgrn.js
@@ -29,15 +29,14 @@
   height: 1200%;\
   width: 1200%;\
   z-index: 999999999;\
-  top: {{negoffset}}px;\
-  left: {{negoffset}}px;\
+  pointer-events: none;\
   animation: sizz 0.1s infinite ease-in-out;\
   -webkit-animation: sizz 0.1s infinite ease-in-out;\
 }\
   ';
 
   function rend(tpt, vars) {
-    return tpt.replace(new RegExp('\\{\\{([^}]+)\\}\\}','g'), function(m, v) {
+    return tpt.replace(/\{\{([^}]+)\}\}/g, function(m, v) {
       return vars[v];
     });
   }
@@ -88,6 +87,25 @@ var ratio = 1.7014652014652014,
     aura, mouseMoveCB, style,
     auraId = "aura" + new Date().getTime(),
 
+    dirs = [
+      [
+        'top',
+        'left'
+      ],
+      [
+        'top',
+        'right'
+      ],
+      [
+        'bottom',
+        'left'
+      ],
+      [
+        'bottom',
+        'right'
+      ]
+    ],
+
     mgrn = root.mgrn = function() {
 
       if (aura) {
@@ -99,7 +117,8 @@ var ratio = 1.7014652014652014,
       }
 
   
-      var wh = document.documentElement.clientHeight,
+      var dir = dirs[Math.floor(Math.random() * 4)],
+      wh = document.documentElement.clientHeight,
       br = wh*2,
       ww = document.documentElement.clientWidth,
       halfwh = Math.ceil(wh/2),
@@ -107,13 +126,11 @@ var ratio = 1.7014652014652014,
       offset = Math.floor(wh / ratio),
       vars = {
         id: auraId,
-        negoffset: -offset,
         offset: offset,
         blur0: wh / 35,
         blur50: wh / 28,
-        blur100: wh / 7,
-
-      }
+        blur100: wh / 7
+      };
 
       style = document.createElement('style');
       style.appendChild(document.createTextNode(rend(globalCss, vars)));
@@ -121,12 +138,15 @@ var ratio = 1.7014652014652014,
 
       aura = document.createElement('div');
       aura.id = auraId;
+      aura.style[dir[0]] = -offset + 'px';
+      aura.style[dir[1]] = -offset + 'px';
       document.body.appendChild(aura);
 
       wiz();
 
+      var borderProp = 'border-' + dir[0] + '-' + dir[1] + '-radius';
       mouseMoveCB = throttle(function(e) {
-        aura.style['border-top-left-radius'] = Math.min(br - (ww - e.clientX)*1.25, br) + 'px ' + Math.min(br - (wh - e.clientY)*1.25, br) + 'px';
+        aura.style[borderProp] = Math.min(br - (ww - e.clientX)*1.25, br) + 'px ' + Math.min(br - (wh - e.clientY)*1.25, br) + 'px';
       }, 50)
 
       document.addEventListener('mousemove', mouseMoveCB);
