d468ee93bbb74afc94fe883e78019ac5a92e5a0a
james <zetlen@gmail.com>
2014-05-20 09:26:07 -0500

    bugfixing lblb

--- dist/lblb.js
+++ dist/lblb.js
@@ -2,7 +2,7 @@
 
 root.lblb = function (sel, rval) {
   rval = rval === "schwa" ? String.fromCharCode(601) : rval || '';
-  return Array.prototype.slice.call(document.querySelectorAll(sel)).forEach(function(node) {
+  return document.querySelectorAll(sel).forEach(function(node) {
     for (var b = document.createTreeWalker(node, NodeFilter.SHOW_TEXT), c, a, d = /[aeiou]/gi; b.nextNode();) a = b.currentNode, c = a.nodeValue, a.nodeValue = c.replace(d, rval)
   });
 };
