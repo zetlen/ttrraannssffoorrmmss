de03ed73106a7248afa05bd2dea9d5bf91693e2a
james <zetlen@gmail.com>
2014-05-20 16:22:52 -0500

    building new lblb

--- dist/lblb.js
+++ dist/lblb.js
@@ -29,7 +29,7 @@
 
 return function (sel, rval) {
   rval = rval === "schwa" ? String.fromCharCode(601) : rval || '';
-  return document.querySelectorAll(sel).forEach(function(node) {
+  return [].slice.call(document.querySelectorAll(sel)).forEach(function(node) {
     for (var b = document.createTreeWalker(node, NodeFilter.SHOW_TEXT), c, a, d = /[aeiou]/gi; b.nextNode();) a = b.currentNode, c = a.nodeValue, a.nodeValue = c.replace(d, rval)
   });
 };
