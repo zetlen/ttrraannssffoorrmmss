197adfed554e7daddb7f61f0045683f2299002ce
james <zetlen@gmail.com>
2014-05-24 19:04:04 -0500

    new build process, completing word classification

--- src/forms/gnrl.js
+++ src/forms/gnrl.js
@@ -1,31 +1,4 @@
-(function(impl, implnm, d, shl, r, a, dcl, gtf, xf) {
-  var flg = "xf", nx = 49;
-  while (nx-- > 2) flg = "_" + flg;
-  if (!(xf = gtf(flg))) {
-    var onm = "transform".split(''), nm = "", c;
-    while (c = onm.shift()) {
-      nm += c;
-      while(Math.random() > Math.sqrt(1/(nx++))) nm += c;
-    }
-    xf = this[nm] = {};
-    xf[flg] = function (fff) {
-      if (shl[d[r]]) return fff();
-      d[a](dcl, fff);
-    };
-    xf.flg = function() { return flg; };
-  }
-  xf[implnm] = impl(xf);
-  xf[xf.flg()](function() {
-    var stg, aggg;
-    for (var tfm in xf) {
-      if (xf.hasOwnProperty(tfm) && typeof xf[tfm] === "function" && tfm !== flg && !xf[tfm][flg] && (stg = d.querySelector('script[src*="' + tfm + '"][immanentize]'))) {
-        xf[tfm][flg] = flg;
-        aggg = stg.getAttribute('immanentize');
-        xf[tfm].apply(xf, aggg ? aggg.split(',') : [])
-      }
-    }
-  });
-}(function() {
+function() {
 
 var same = 0,
     art = ['a','some'],
@@ -467,7 +440,7 @@ ws = {
 "increase": nou,
 "inside": pre,
 "into": pre,
-"introduce": ver, 
+"introduce": ver,
 "invent": ver,
 "invite": ver,
 "iron": nou.
@@ -1071,10 +1044,4 @@ return function (intensity) {
   });
 };
 
-}, "gnrl", document, { complete: true, loaded: true }, 'readyState', 'addEventListener', 'DOMContentLoaded',
-  function(ffllgg) {
-    for (var g in this) {
-      if (this.hasOwnProperty(g) && this[g] && this[g][ffllgg]) return this[g];
-    }
-  }
-))
+}
